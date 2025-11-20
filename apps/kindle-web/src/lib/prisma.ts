db10f89a53ee27fa107989e95b43430eee40007d
hardikmodi1 <hmodi2457@gmail.com>
2025-11-20 23:17:37 +0530

    [web] chore: upgrade prisma to v7

--- apps/kindle-web/src/lib/prisma.ts
+++ apps/kindle-web/src/lib/prisma.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from '@/generated/prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma = new PrismaClient({ adapter }).$extends(withAccelerate());
 
 export default prisma;
