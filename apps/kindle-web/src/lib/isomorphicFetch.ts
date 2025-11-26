c1f510967206836d9bec7046602f779a721307bc
hardikmodi1 <hmodi2457@gmail.com>
2025-11-27 00:46:27 +0530

    [web] fix: make queries to work correctly on server

--- apps/kindle-web/src/lib/isomorphicFetch.ts
+++ apps/kindle-web/src/lib/isomorphicFetch.ts
@@ -4,7 +4,7 @@ import { getRequestHeaders } from '@tanstack/react-start/server';
 export const isomorphicFetch = createIsomorphicFn()
   .server(async (input: string | URL | Request, init?: RequestInit) => {
     const headers = getRequestHeaders();
-    return fetch(input, { headers, ...init });
+    return fetch(`${process.env.SERVER_URL}${input}`, { headers, ...init });
   })
   .client(async (input: string | URL | Request, init?: RequestInit) => {
     return fetch(input, init);
